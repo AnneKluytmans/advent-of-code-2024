3af5879fd28c0658181f4157de2fb36235fa9b1b
AnneKluytmans <annekluytmans1@gmail.com>
2025-06-20 12:51:42 +0200

    Set up puzzle input

---
+++ src/main/java/Helpers/CreatePuzzleBoard.java
@@ -0,0 +1,17 @@
+package Helpers;
+
+public class CreatePuzzleBoard {
+    public static char[][] createPuzzleBoard(String puzzleInput) {
+        String[] puzzleBoardRows = puzzleInput.split("\n");
+        int rows = puzzleBoardRows.length;
+        int cols = puzzleBoardRows[0].length();
+
+        char[][] board = new char[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            board[i] = puzzleBoardRows[i].toCharArray();
+        }
+
+        return board;
+    }
+}
