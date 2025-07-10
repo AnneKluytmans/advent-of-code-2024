c6b3b7102945d634b83954d3255aba3e90a3d776
AnneKluytmans <annekluytmans1@gmail.com>
2025-07-10 15:11:16 +0200

    Refactored day 9 part 1

--- src/main/java/Day9DiskFragmenter/FilesystemChecksumCalculator.java
+++ src/main/java/Day9DiskFragmenter/FilesystemChecksumCalculator.java
@@ -6,85 +6,78 @@ import java.util.List;
 public class FilesystemChecksumCalculator {
     public static void main(String[] args) {
         PuzzleInput puzzleInput = new PuzzleInput();
-        List<Integer> digits = toDigitsList(puzzleInput.getPuzzleInput());
+        List<Integer> digits = parseToDigits(puzzleInput.getPuzzleInput());
 
-        Long checksum = calculateChecksum(digits);
+        List<Integer> disk = createDiskLayout(digits);
+        compactDisk(disk);
+
+        long checksum = calculateChecksum(disk);
         System.out.println("Filesystem checksum: " + checksum);
     }
 
-    public static Long calculateChecksum(List<Integer> digits) {
+    public static long calculateChecksum(List<Integer> disk) {
         long checksum = 0L;
-        List<Integer> sortedDigitBlocks = toSortedDigitBlocks(digits);
-        List<Integer> parsedDigits = backFillDotsWithDigits(sortedDigitBlocks);
 
-        for (int i = 0; i < parsedDigits.size(); i++) {
-            checksum += parsedDigits.get(i) * i;
+        for (int i = 0; i < disk.size(); i++) {
+            Integer fieldId = disk.get(i);
+            if (fieldId != null) {
+                checksum += (long) fieldId * i;
+            }
         }
         return checksum;
     }
 
-    private static List<Integer> toSortedDigitBlocks(List<Integer> digits) {
-        List<Integer> blocks = new ArrayList<>();
+    private static List<Integer> createDiskLayout(List<Integer> digitBlocks) {
+        List<Integer> disk = new ArrayList<>();
         int fileId = 0;
 
-        for (int i = 0; i < digits.size(); i++) {
+        for (int i = 0; i < digitBlocks.size(); i++) {
             if (i % 2 == 0) {
-                for (int j = 0; j < digits.get(i); j++) {
-                    blocks.add(fileId);
+                for (int j = 0; j < digitBlocks.get(i); j++) {
+                    disk.add(fileId);
                 }
                 fileId++;
             } else {
-                for (int j = 0; j < digits.get(i); j++) {
-                    blocks.add(null);
+                for (int j = 0; j < digitBlocks.get(i); j++) {
+                    disk.add(null);
                 }
             }
         }
-        return blocks;
+        return disk;
     }
 
-    public static List<Integer> backFillDotsWithDigits(List<Integer> digitBlocks) {
-        List<Integer> blocks = new ArrayList<>(digitBlocks);
-
+    public static void compactDisk(List<Integer> disk) {
         while (true) {
-            int firstDot = -1;
-            for (int i = 0; i < blocks.size(); i++) {
-                if (blocks.get(i) == null) {
-                    firstDot = i;
+            int firstFreeSpace = -1;
+            for (int i = 0; i < disk.size(); i++) {
+                if (disk.get(i) == null) {
+                    firstFreeSpace = i;
                     break;
                 }
             }
-            if (firstDot == -1) break;
+            if (firstFreeSpace == -1) break;
 
             int lastDigit = -1;
-            for (int i = blocks.size() -1; i > firstDot; i--) {
-                if (blocks.get(i) != null) {
+            for (int i = disk.size() -1; i > firstFreeSpace; i--) {
+                if (disk.get(i) != null) {
                     lastDigit = i;
                     break;
                 }
             }
             if (lastDigit == -1) break;
 
-            blocks.set(firstDot, blocks.get(lastDigit));
-            blocks.set(lastDigit, null);
+            disk.set(firstFreeSpace, disk.get(lastDigit));
+            disk.set(lastDigit, null);
         }
-
-        List<Integer> result = new ArrayList<>();
-        for (Integer n : blocks) {
-            if (n != null) {
-                result.add(n);
-            }
-        }
-
-        return result;
     }
 
-    public static List<Integer> toDigitsList(String input) {
-        List<Integer> digitsList = new ArrayList<>();
+    private static List<Integer> parseToDigits(String input) {
+        List<Integer> digits = new ArrayList<>();
 
         for (char c : input.toCharArray()) {
-            digitsList.add(Character.getNumericValue(c));
+            digits.add(Character.getNumericValue(c));
         }
-        return digitsList;
+        return digits;
     }
 
 }
