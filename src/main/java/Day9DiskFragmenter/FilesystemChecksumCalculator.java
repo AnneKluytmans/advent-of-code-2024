cf715daa1b6edc25886f528541f8196d1a3ab593
AnneKluytmans <annekluytmans1@gmail.com>
2025-07-11 15:37:01 +0200

    Solved day 9 part 2

--- src/main/java/Day9DiskFragmenter/FilesystemChecksumCalculator.java
+++ src/main/java/Day9DiskFragmenter/FilesystemChecksumCalculator.java
@@ -2,6 +2,7 @@ package Day9DiskFragmenter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class FilesystemChecksumCalculator {
     public static void main(String[] args) {
@@ -9,10 +10,13 @@ public class FilesystemChecksumCalculator {
         List<Integer> digits = parseToDigits(puzzleInput.getPuzzleInput());
 
         List<Integer> disk = createDiskLayout(digits);
-        compactDisk(disk);
+        List<Integer> disk1 = compactDiskByShiftingFragments(disk);
+        List<Integer> disk2 = compactDiskByShiftingWholeFiles(disk);
 
-        long checksum = calculateChecksum(disk);
-        System.out.println("Filesystem checksum: " + checksum);
+        long checksum1 = calculateChecksum(disk1);
+        long checksum2 = calculateChecksum(disk2);
+        System.out.println("Filesystem checksum part 1: " + checksum1);
+        System.out.println("Filesystem checksum part 2: " + checksum2);
     }
 
     public static long calculateChecksum(List<Integer> disk) {
@@ -46,11 +50,13 @@ public class FilesystemChecksumCalculator {
         return disk;
     }
 
-    public static void compactDisk(List<Integer> disk) {
+    public static List<Integer> compactDiskByShiftingFragments(List<Integer> disk) {
+        List<Integer> compactDisk = new ArrayList<>(disk);
+
         while (true) {
             int firstFreeSpace = -1;
             for (int i = 0; i < disk.size(); i++) {
-                if (disk.get(i) == null) {
+                if (compactDisk.get(i) == null) {
                     firstFreeSpace = i;
                     break;
                 }
@@ -59,16 +65,65 @@ public class FilesystemChecksumCalculator {
 
             int lastDigit = -1;
             for (int i = disk.size() -1; i > firstFreeSpace; i--) {
-                if (disk.get(i) != null) {
+                if (compactDisk.get(i) != null) {
                     lastDigit = i;
                     break;
                 }
             }
             if (lastDigit == -1) break;
 
-            disk.set(firstFreeSpace, disk.get(lastDigit));
-            disk.set(lastDigit, null);
+            compactDisk.set(firstFreeSpace, compactDisk.get(lastDigit));
+            compactDisk.set(lastDigit, null);
         }
+
+        return compactDisk;
+    }
+
+    public static List<Integer> compactDiskByShiftingWholeFiles(List<Integer> disk) {
+        List<Integer> compactDisk = new ArrayList<>(disk);
+        int highestId = 0;
+
+        for (int i = compactDisk.size() - 1; i > 0; i--) {
+            if (compactDisk.get(i) != null) {
+                highestId = compactDisk.get(i);
+                break;
+            }
+        }
+
+        for (int fileId = highestId; fileId > 0; fileId--) {
+            int fileStart = compactDisk.indexOf(fileId);
+            int fileEnd = fileStart;
+
+            for (int i = fileStart; i < compactDisk.size(); i++) {
+                if (!Objects.equals(compactDisk.get(i), fileId)) {
+                    fileEnd = i - 1;
+                    break;
+                }
+                fileEnd = i;
+            }
+
+            int fileLength = fileEnd - fileStart + 1;
+
+            for (int i = 0; i <= fileStart; i++) {
+                boolean fits = true;
+                for (int j = 0; j < fileLength; j++) {
+                    if (compactDisk.get(i + j) != null) {
+                        fits = false;
+                        break;
+                    }
+                }
+
+                if (fits) {
+                    for (int j = 0; j < fileLength; j++) {
+                        compactDisk.set(i + j, fileId);
+                        compactDisk.set(fileStart + j, null);
+                    }
+                    break;
+                }
+            }
+        }
+
+        return compactDisk;
     }
 
     private static List<Integer> parseToDigits(String input) {
