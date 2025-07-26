a3488a6c5e026ff608c6a7edb1f8839c0853b608
AnneKluytmans <annekluytmans1@gmail.com>
2025-07-26 11:42:34 +0200

    Solved day 12 part 2

--- src/main/java/Day12GardenGroups/GardenFencingPriceCalculator.java
+++ src/main/java/Day12GardenGroups/GardenFencingPriceCalculator.java
@@ -10,28 +10,47 @@ public class GardenFencingPriceCalculator {
         PuzzleInput puzzleInput = new PuzzleInput();
         char[][] puzzleBoard = puzzleInput.getPuzzleBoard();
 
-        long totalPrice = calculateTotalFencingPrice(puzzleBoard);
+        List<Region> regions = getRegions(puzzleBoard);
+
+        long totalPrice = calculateTotalFencingPrice(regions);
+        long discountPrice = calculateDiscountFencingPrice(regions);
         System.out.println("The total fencing price is: " + totalPrice);
+        System.out.println("The total discount price is: " + discountPrice);
     }
 
     public record Point(int row, int col) {}
 
-    public static long calculateTotalFencingPrice(char[][] puzzleBoard) {
-        long totalPrice = 0;
-        Set<Point> visited = new HashSet<>();
-        List<Region> regions = new ArrayList<>();
+    public record Edge(Point inside, Point outside) {
 
-        for (int i = 0; i < puzzleBoard.length; i++) {
-            for (int j = 0; j < puzzleBoard[0].length; j++) {
-                Point p = new Point(i, j);
-                if (!visited.contains(p)) {
-                    Region region = new Region();
-                    char target = puzzleBoard[i][j];
-                    scanRegion(puzzleBoard, i, j, visited, region, target);
-                    regions.add(region);
-                }
-            }
+        public boolean isHorizontal() {
+            return inside.col == outside.col && inside.row != outside.row;
         }
+
+        public boolean isVertical() {
+            return inside.row == outside.row && inside.col != outside.col;
+        }
+
+        public Edge nextInSameDirection() {
+            if (isVertical()) {
+                return new Edge(new Point(inside.row + 1, inside.col), new Point(outside.row + 1, outside.col));
+            } else if (isHorizontal()) {
+                return new Edge(new Point(inside.row, inside.col + 1), new Point(outside.row, outside.col + 1));
+            }
+            return null;
+        }
+
+        public Edge previousInSameDirection() {
+            if (isVertical()) {
+                return new Edge(new Point(inside.row - 1, inside.col), new Point(outside.row - 1, outside.col));
+            } else if (isHorizontal()) {
+                return new Edge(new Point(inside.row, inside.col - 1), new Point(outside.row, outside.col - 1));
+            }
+            return null;
+        }
+    }
+
+    public static long calculateTotalFencingPrice(List<Region> regions) {
+        long totalPrice = 0;
 
         for (Region region : regions) {
             totalPrice += region.getFencingPrice();
@@ -40,7 +59,38 @@ public class GardenFencingPriceCalculator {
         return totalPrice;
     }
 
-    public static void scanRegion(char[][] puzzleBoard, int row, int col, Set<Point> visited, Region region, char target) {
+    public static long calculateDiscountFencingPrice(List<Region> regions) {
+        long discountPrice = 0;
+
+        for (Region region : regions) {
+            discountPrice += region.getDiscountFencingPrice();
+        }
+
+        return discountPrice;
+    }
+
+    public static List<Region> getRegions(char[][] puzzleBoard) {
+        Set<Point> visited = new HashSet<>();
+        List<Region> regions = new ArrayList<>();
+
+        for (int i = 0; i < puzzleBoard.length; i++) {
+            for (int j = 0; j < puzzleBoard[0].length; j++) {
+                Point p = new Point(i, j);
+                if (!visited.contains(p)) {
+                    Region region = new Region();
+                    Set<Edge> edges = new HashSet<>();
+                    char target = puzzleBoard[i][j];
+                    scanRegion(puzzleBoard, i, j, visited, region, target, edges);
+                    scanEdges(edges, region);
+                    regions.add(region);
+                }
+            }
+        }
+
+        return regions;
+    }
+
+    public static void scanRegion(char[][] puzzleBoard, int row, int col, Set<Point> visited, Region region, char target, Set<Edge> edges) {
         Point current = new Point(row, col);
         if (visited.contains(current)) return;
 
@@ -54,17 +104,44 @@ public class GardenFencingPriceCalculator {
             int newRow = row + dy[d];
             int newCol = col + dx[d];
 
+            Edge edge = new Edge(new Point(row, col), new Point(newRow, newCol));
             if (newRow >= 0 && newRow < puzzleBoard.length &&
                 newCol >= 0 && newCol < puzzleBoard[0].length) {
 
                 if (puzzleBoard[newRow][newCol] == target) {
-                    scanRegion(puzzleBoard, newRow, newCol, visited, region, target);
+                    scanRegion(puzzleBoard, newRow, newCol, visited, region, target, edges);
                 } else {
                     region.increasePerimeter();
+                    edges.add(edge);
                 }
             } else {
                 region.increasePerimeter();
+                edges.add(edge);
             }
         }
+    }
+
+    public static void scanEdges(Set<Edge> edges, Region region) {
+        Set<Edge> visited = new HashSet<>();
+
+        for (Edge edge : edges) {
+            if (visited.contains(edge)) continue;
+
+            region.increaseSides();
+            visited.add(edge);
+
+            Edge nextEdge = edge.nextInSameDirection();
+            while (nextEdge != null && edges.contains(nextEdge) && !visited.contains(nextEdge)) {
+                visited.add(nextEdge);
+                nextEdge = nextEdge.nextInSameDirection();
+            }
+
+            Edge previousEdge = edge.previousInSameDirection();
+            while (previousEdge != null && edges.contains(previousEdge) && !visited.contains(previousEdge)) {
+                visited.add(previousEdge);
+                previousEdge = previousEdge.previousInSameDirection();
+            }
+        }
+
     }
 }
--- src/main/java/Day12GardenGroups/Region.java
+++ src/main/java/Day12GardenGroups/Region.java
@@ -3,12 +3,14 @@ package Day12GardenGroups;
 public class Region {
     private int area = 0;
     private int perimeter = 0;
+    private int sides = 0;
 
     public Region() {}
 
-    public Region(int area, int perimeter) {
+    public Region(int area, int perimeter, int sides) {
         this.area = area;
         this.perimeter = perimeter;
+        this.sides = sides;
     }
 
     public int getArea() {
@@ -19,8 +21,16 @@ public class Region {
         return perimeter;
     }
 
+    public int getSides() {
+        return sides;
+    }
+
     public long getFencingPrice() {
         return (long) area * perimeter;
+    }
+
+    public long getDiscountFencingPrice() {
+        return (long) area * sides;
     }
 
     public void setArea(int area) {
@@ -31,11 +41,19 @@ public class Region {
         this.perimeter = perimeter;
     }
 
+    public void setSides(int sides) {
+        this.sides = sides;
+    }
+
     public void increaseArea() {
         this.area++;
     }
 
     public void increasePerimeter() {
         this.perimeter++;
+    }
+
+    public void increaseSides() {
+        this.sides++;
     }
 }
